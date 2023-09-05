546979dba95a78abd7a54311f248708e5ce75fea
Bubzone <kubaszmajdzinski@gmail.com>
2023-09-05 09:45:07 +0200

    Kręcenie o losowy int

--- roll.js
+++ roll.js
@@ -1,5 +1,21 @@
+function randomDeg(){
+    rand=(Math.random()*(1080-360))+360
+    
+    console.log("losuje")
+    return rand
+}
 function roll() {
-    let x = document.getElementById("box")
-    x.style.rotate = "45deg"
+    let x = document.getElementById("box") 
+    x.style.rotate="0deg"
+    deg=randomDeg()
+    text=deg.toString()
+    text+="deg"
+    console.log(text)
+    x.style.rotate = text
     x.style.transition = "2s"
+}
+function reset() {
+    let x = document.getElementById("box")
+    x.style.rotate="0deg"
+    x.style.transition = "0s"
 }
